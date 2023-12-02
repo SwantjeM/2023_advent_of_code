a7a6be6afaa6a89c0021e76139cfe9a478b8f304
Swantje Moehle <28712729+SwantjeM@users.noreply.github.com>
2023-12-02 00:12:00 -0800

    Part 1 done! took forever to find the stupid typo at 'blue = int(blue)' instead of 'blue = int(cube)' I spent way too long searching for this!!!

--- day2/day2.py
+++ day2/day2.py
@@ -1,22 +1,11 @@
 # Determine which games would have been possible if the bag had been loaded with only
 # 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
 
-import re
-
-red_goal = 12
-green_goal = 13
-blue_goal = 14
-
 
 class Game:
     def __init__(self, ID=None, set1=None, set2=None, set3=None):
         self.ID = ID
-        self.set1 = set1
-        self.set2 = set2
-        self.set3 = set3
-
-    def __str__(self):
-        return f"Game ID: {self.ID}, Set1: {self.set1}, Set2: {self.set2}, Set3: {self.set3}"
+        self.sets = []
 
 
 test_input = [
@@ -30,27 +19,30 @@ test_input = [
 
 def pattern_matching(input_string):
     game = Game
-    # patter matching:
-    # (\d+) : one or more digits
-    # ([\w\d\s,]+) : one or more occurrences of word characters, digits, spaces, and commas
-    # \s*;\s* : zero or more whitespaces surrounding a semicolon
-    pattern = r"Game (\d+): ([\w\d\s,]+)\s*;\s*([\w\d\s,]+)\s*;\s*([\w\d\s,]+)"
-    match = re.match(pattern, input_string)
+    first_split = input_string.split(":")
+    print(first_split[0])
+    print(first_split[1])
 
-    if match:
-        # Extract the groups
-        game.ID = match.group(1)
-        game.set1 = match.group(2)
-        game.set2 = match.group(3)
-        game.set3 = match.group(4)
+    if "Game " in first_split[0]:
+        game.ID = first_split[0].replace("Game ", "")
+        print(game.ID)
     else:
-        print("No match found in: ", input_string)
+        print("Error, no Game in ", first_split[0])
+
+    game.sets = first_split[1].split(";")
+    print(game.sets)
+
     return game
 
 
 def parse_set(set):
+    print(set)
     raw_cubes = set.split(",")
+    red = 0
+    green = 0
+    blue = 0
     for cube in raw_cubes:
+        print(cube)
         if " red" in cube:
             cube = cube.replace(" red", "")
             red = int(cube)
@@ -59,26 +51,59 @@ def parse_set(set):
             green = int(cube)
         elif " blue" in cube:
             cube = cube.replace(" blue", "")
-            blue = int(blue)
+            blue = int(cube)
+            print("BLUE!", blue)
         else:
             print("error parsing cube")
-    return red, green, blue
+        print(cube)
+        print("r g b", red, green, blue)
+    return [red, green, blue]
 
 
-def check_set(red, green, blue):
-    possible = True
-    if (red > 12) and (green > 13) and (blue > 14):
+def check_set(colors):
+    red = colors[0]
+    green = colors[1]
+    blue = colors[2]
+    possible = False
+    if (red <= 12) and (green <= 13) and (blue <= 14):
         possible = True
     return possible
 
 
-file_path = "day1_inputs.txt"
+file_path = "day2_inputs.txt"
 
-# with open(file_path, "r") as file:
-#     input = [line.strip() for line in file]
+with open(file_path, "r") as file:
+    input = [line.strip() for line in file]
 
-input = "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
-test_game = pattern_matching(input)
+# input = [
+#     "Game 82: 2 green, 7 blue, 2 red; 15 blue, 2 green, 1 red; 3 blue, 2 green; 1 red; 2 red, 15 blue, 2 green"
+# ]
+
+poss = []
+result = 0
+for line in input:
+    # input = "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
+    test_game = pattern_matching(line)
+    possible = []
+    for set in test_game.sets:
+        possible.append(check_set(parse_set(set)))
+    # print(possible)
+
+    if all(possible):
+        print(possible)
+        print("Game: ", test_game.ID, " is possible")
+        result = result + int(test_game.ID)
+        print(int(test_game.ID))
+        print(result)
+        poss.append(int(test_game.ID))
+
+    else:
+        # print("Game: ", test_game.ID, " is impossible")
+        pass
+
+print(result)
+print(poss)
+print(sum(poss))
 
 
-print(test_game)
+# print(test_game)
